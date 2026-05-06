75e256e8df4b787d3006ba1a576ec40b2ab87bc8
Morbanaa <theodorerodd@gmail.com>
2026-05-06 17:35:30 -0500

    Set constants for width and height and placed the buttons in set locations. Also finished the show info method so it fills in the info

--- classes.py
+++ classes.py
@@ -1,30 +1,41 @@
+# Teddy Rodd
+# Morbanaa Studios
+# Name and Address
+
 from tkinter import *
-from turtle import back
 
 class My_Gui:
+    width = 800
+    height = 400
     def __init__(self):
+        
+        # Window Set Up
         self.window = Tk()
         self.window.title("Name and Address Program")
-        self.window.geometry("800x800")
+        self.window.geometry(f"{self.width}x{self.height}")
         self.icon = PhotoImage(file="M.png")
         self.window.iconphoto(True,self.icon)
         self.window.config(background="grey")
+
+        # Labels
+        self.label = Label(self.window,text="") # Starts Empty
+        self.label.pack()
 
         # Show Info Button
         self.button_info = Button(self.window,
                              text="Show Info",
                              command=self.show_info,
                              font=('arial',50))
-        self.button_info.pack()
+        self.button_info.place(x=50,y=self.height-200)
 
         # Exit Button
         self.button_info = Button(self.window,
                             text="Exit",
                             command=self.window.destroy,
                             font=('arial',50))
-        self.button_info.pack()
+        self.button_info.place(x=self.width-200,y=self.height-200)
 
         self.window.mainloop()
 
-    def show_info():
-        pass
+    def show_info(self):
+        self.label.config(text="Teddy Rodd\n408 West Jefferson Ave\nWheaton Il 60187")
